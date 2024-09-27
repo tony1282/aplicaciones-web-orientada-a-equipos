704ce1fe59df2b629107d96795654dfa1b647914
antony <antonydelacruzramos@gmail.com>
2024-09-26 20:31:54 -0600

    correcion 02

---
+++ concepts/variables.js
@@ -0,0 +1,20 @@
+let name;
+console.log(name);
+name = "Peter";
+console.log(name);
+console.log(typeof name);
+name = 18;
+console.log(name);
+console.log(typeof name);
+
+const PI = 3.1416;
+console.log(PI);
+console.log(typeof PI);
+// PI = 3.15; // Error
+
+console.log("----------------");
+console.log(sumar(10, 20))
+
+function sumar(a, b) {
+    return a + b;
+}
