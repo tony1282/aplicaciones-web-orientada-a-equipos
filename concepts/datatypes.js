894ab80be1c2ee561086e9ecb886b08a6c741ba2
antony <antonydelacruzramos@gmail.com>
2024-09-26 20:29:10 -0600

    correcion 01

--- concepts/datatypes.js
+++ concepts/datatypes.js
@@ -1,14 +1,18 @@
+console.log(24);
 console.log(typeof 24);
-let name;
-console.log(name);
-name = "peter"
-console.log(typeof name);
-name = 18; 
-console.log(name);
-console.log(typeof name);
-
-const PI = 3.1416;
-console.log(PI)
-console.log(typeof PI)
-//PI = 3.15;//error
-
+console.log(2.3);
+console.log(typeof 2.3);
+console.log(false);
+console.log(typeof false);
+console.log("Hola mundo!");
+console.log(typeof "Hola mundo!");
+console.log("c");
+console.log(typeof "c");
+console.log(null);
+console.log(typeof null);
+console.log([1,2,3]);
+console.log(typeof [1,2,3]);
+console.log({nombre: "Antony"});
+console.log(typeof {nombre: "Antony"});
+console.log(undefined);
+console.log(typeof undefined);
